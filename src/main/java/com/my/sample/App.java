f1bcc0222809261bcbe5cdddcb786e48bcd312d4
TT <kjp7355@naver.com>
2017-07-26 10:37:47 +0900

    Commit do it

--- src/main/java/com/my/sample/App.java
+++ src/main/java/com/my/sample/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!Hello World" );
         System.out.println("Insert~!!");
+        System.out.println( "Hello World!Hello World" );
     }
 }
