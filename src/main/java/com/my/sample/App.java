777135cc63961fb54572bca4d4bc5de5fa053396
kTaeHyung <kjp7355@naver.com>
2017-07-26 11:36:34 +0900

    Update App.java

--- src/main/java/com/my/sample/App.java
+++ src/main/java/com/my/sample/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!Hello World" );
-        System.out.println("Insert~!!sssssssssss");
+        System.out.println("Insert~!!");
     }
 }
