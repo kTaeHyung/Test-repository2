124ee61a93b2716e804a37ad1e92537f5257ae17
TT <kjp7355@naver.com>
2017-07-26 09:58:49 +0900

    Commit do it

---
+++ src/main/java/com/my/sample/App.java
@@ -0,0 +1,14 @@
+package com.my.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Insert~!!");
+    }
+}
